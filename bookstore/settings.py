37084ca164e241787a87d7b0d792ef93f2c7b0c6
Helbert guirra <helbert.guirra@gmail.com>
2026-04-08 09:56:44 -0300

    fix: configurar settings para deploy no Render

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -155,10 +155,11 @@ INTERNAL_IPS = [
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "chave-local-insegura-apenas-dev")
 
-DEBUG = int(os.environ.get("DEBUG", default=1))
+DEBUG = os.environ.get("DEBUG", "0") == "1"
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost").split(" ")
+
 
 
